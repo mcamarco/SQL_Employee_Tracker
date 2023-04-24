6d690497613ad0bc358ac169f5d206c6f719a3b1
Mickey Camarco <48422179+mcamarco@users.noreply.github.com>
2023-04-24 18:31:57 -0400

    updated query.sql

--- db/query.sql
+++ db/query.sql
@@ -1,6 +1,19 @@
--- TODO: CHANGE TO EMPLOYEE TRACKING PROJECT - NOT MOVIES
-SELECT movies.movie_name AS movie, reviews.review
-FROM reviews
-LEFT JOIN movies
-ON reviews.movie_id = movies.id
-ORDER BY movies.movie_name;
+SELECT
+    employee.id,
+    CONCAT(
+        employee.first_name,
+        ' ',
+        employee.last_name
+    ) AS employee_name,
+    role.title,
+    department.name AS department,
+    role.salary,
+    CONCAT(
+        manager.first_name,
+        ' ',
+        manager.last_name
+    ) AS manager_name
+FROM employee
+    LEFT JOIN role ON employee.role_id = role.id
+    LEFT JOIN department ON role.department_id = department.id
+    LEFT JOIN employee AS manager ON employee.manager_id = manager.id;
