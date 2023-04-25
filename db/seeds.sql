c257aa36e901509819f7aac7ad2eadd516add768
Mickey Camarco <48422179+mcamarco@users.noreply.github.com>
2023-04-24 21:22:29 -0400

    realized sever.js was not needed for this project, reset file structure for index.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,16 @@
 -- TODO: CHANGE TO EMPLOYEE TRACKING PROJECT - NOT MOVIES
-INSERT INTO movies (movie_name)
-VALUES ("Lion King"),
-       ("The Godfather"),
-       ("West Side Story"),
-       ("Parasite"),
-       ("The Wizard of Oz");
+INSERT INTO employees_db (employees)
+VALUES ("Stevie Budd"),
+       ("Twyla Sands"),
+       ("Alexis Rose"),
+       
 
-INSERT INTO reviews (movie_id, review)
-VALUES (1, "Zazu is underrated. Give that hornbill a sequel!"),
-       (2, "I'm gonna make him an offer you can't refuse, watch this movie"),
-       (1, "Scar is the lion everyone loves to hate"),
-       (3, "Ten years of ballet and three years of tap to join a gang in this neighborhood"),
-       (5, "The tin man gave a metallic, hollow performance"),
-       (1, "Hakuna matata"),
-       (5, "Those flying monkeys are nightmare fuel!");
+-- INSERT INTO reviews (movie_id, review)
+-- VALUES (1, "Zazu is underrated. Give that hornbill a sequel!"),
+--        (2, "I'm gonna make him an offer you can't refuse, watch this movie"),
+--        (1, "Scar is the lion everyone loves to hate"),
+--        (3, "Ten years of ballet and three years of tap to join a gang in this neighborhood"),
+--        (5, "The tin man gave a metallic, hollow performance"),
+--        (1, "Hakuna matata"),
+--        (5, "Those flying monkeys are nightmare fuel!");
        
