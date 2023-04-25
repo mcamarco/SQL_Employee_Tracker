40ef7c4b80de85e53fc992246ee93d1cdf11dbc3
Mickey Camarco <48422179+mcamarco@users.noreply.github.com>
2023-04-24 21:41:50 -0400

    updated index.js

--- index.js
+++ index.js
@@ -57,70 +57,72 @@ function promptUser() {
 
 promptUser();
 
-function updateEmployeeRole() {
-  inquirer
-    .prompt([
-      {
-        type: 'input',
-        name: 'title',
-        message: 'Enter the new role title:'
-      },
-      {
-        type: 'input',
-        name: 'salary',
-        message: 'Enter the new role salary:'
-      },
-      {
-        type: 'input',
-        name: 'department_id',
-        message: 'Enter the new role department ID:'
-      }
-    ])
-    .then((answers) => {
-      db.query(
-        `UPDATE roles SET title = ?, salary = ?, department_id = ? WHERE id = ?`,
-        [answers.title, answers.salary, answers.department_id, roleId],
-        (err, result) => {
-          if (err) throw err;
-          console.log(`Role ${roleId} updated successfully.`);
-          promptUser();
-        }
-      );
-    });
-}
+// function updateEmployeeRole() {
+//   inquirer
+//     .prompt([
+//       {
+//         type: 'input',
+//         name: 'title',
+//         message: 'Enter the new role title:'
+//       },
+//       {
+//         type: 'input',
+//         name: 'salary',
+//         message: 'Enter the new role salary:'
+//       },
+//       {
+//         type: 'input',
+//         name: 'department_id',
+//         message: 'Enter the new role department ID:'
+//       }
+//     ])
+//     .then((answers) => {
+//       db.query(
+//         `UPDATE roles SET title = ?, salary = ?, department_id = ? WHERE id = ?`,
+//         [answers.title, answers.salary, answers.department_id, roleId],
+//         (err, result) => {
+//           if (err) throw err;
+//           console.log(`Role ${roleId} updated successfully.`);
+//           promptUser();
+//         }
+//       );
+//     });
+// }
 
-function viewAllRoles() {
-  db.query(viewAllRolesQuery, (err, result) => {
-    if (err) throw err;
-    console.table(result);
-    promptUser();
-  });
-}
+// function viewAllRoles() {
+//   db.query(viewAllRolesQuery, (err, result) => {
+//     if (err) throw err;
+//     console.table(result);
+//     promptUser();
+//   });
+// }
 
-function addARole() {
-  inquirer
-    .prompt([
-      {
-        type: 'input',
-        name: 'title',
-        message: 'Enter the role title:'
-      },
-      {
-        type: 'input',
-        name: 'salary',
-        message: 'Enter the role salary:'
-      },
-      {
-        type: 'input',
-        name: 'department_id',
-        message: 'Enter the role department ID:'
-      }
-    ])
-    .then((answers) => {
-      db.query(
-        `INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)`,
-        [answers.title, answers.salary, answers.department_id],
-        (err, result) => {
-          if (err) throw err;
-          console.log(`Role ${result.insertId} added successfully.`);
-          prompt
+// function addARole() {
+//   inquirer
+//     .prompt([
+//       {
+//         type: 'input',
+//         name: 'title',
+//         message: 'Enter the role title:'
+//       },
+//       {
+//         type: 'input',
+//         name: 'salary',
+//         message: 'Enter the role salary:'
+//       },
+//       {
+//         type: 'input',
+//         name: 'department_id',
+//         message: 'Enter the role department ID:'
+//       }
+//     ])
+//     .then((answers) => {
+//       db.query(
+//         `INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)`,
+//         [answers.title, answers.salary, answers.department_id],
+//         (err, result) => {
+//           if (err) throw err;
+//           console.log(`Role ${result.insertId} added successfully.`);
+//           prompt
+//         })
+//     });
